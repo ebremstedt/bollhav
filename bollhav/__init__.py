6dce788e2d704517b611b64f160cca7ee2f61caa
ebremst3dt <erik.bremstedt-pedersen@regionstockholm.se>
2026-02-25 08:39:39 +0100

    gall

--- bollhav/__init__.py
+++ bollhav/__init__.py
@@ -1,10 +1,16 @@
 from bollhav.model import Model
 from bollhav.modes import ModelType, WriteMode
 from bollhav.batching import BatchSize
+from bollhav.database import Database, DatabaseColumn
+from bollhav.postgres import PostgresColumn, PostgresType
 
 __all__ = [
     "Model",
     "ModelType",
     "WriteMode",
     "BatchSize",
+    "Database",
+    "DatabaseColumn",
+    "PostgresColumn",
+    "PostgresType",
 ]
