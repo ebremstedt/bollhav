da4d8a78e52bf4c1029e8f473edf6d9ebb211f38
ebremst3dt <erik.bremstedt-pedersen@regionstockholm.se>
2026-03-02 13:15:03 +0100

    gall

--- bollhav/modes.py
+++ bollhav/modes.py
@@ -6,7 +6,7 @@ class WriteMode(Enum):
     TRUNCATE_INSERT = "TRUNCATE_INSERT"  # Fully load a table each time
     UPDATE_INSERT = "UPDATE_INSERT"  # Update and/or insert, but do not delete
     OVERWRITE_INSERT = "OVERWRITE_INSERT"  # Update and/or insert, but _do_ delete first
-    MERGE = "MERGE"  # Wait until postgres 15
+    # MERGE = "MERGE"  # Wait until postgres 15
     VIEW = "VIEW"  # Simply update view in place
 
 
