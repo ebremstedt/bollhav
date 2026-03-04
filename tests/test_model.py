14a0e3078c791a91d69fb548f076948d447edd69
ebremst3dt <erik.bremstedt-pedersen@regionstockholm.se>
2026-03-04 16:25:46 +0100

    gall

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,13 @@
-from unittest.mock import MagicMock, patch
-import polars as pl
-import pytest
-from bollhav.postgres.write_modes import write
+# from unittest.mock import MagicMock, patch
+# import polars as pl
+# import pytest
+# from bollhav.postgres.write_modes import write
 
 
-def test_invalid_write_mode():
-    conn = MagicMock()
-    model = MagicMock()
-    model.write_mode = MagicMock()
+# def test_invalid_write_mode():
+#     conn = MagicMock()
+#     model = MagicMock()
+#     model.write_mode = MagicMock()
 
-    with pytest.raises(ValueError):
-        write(conn=conn, df_gen=iter([pl.DataFrame({"a": [1]})]), model=model)
+#     with pytest.raises(ValueError):
+#         write(conn=conn, df_gen=iter([pl.DataFrame({"a": [1]})]), model=model)
