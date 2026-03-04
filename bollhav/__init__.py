21666f272d647a6c7bb4c9cfd85a438a7becd847
ebremst3dt <erik.bremstedt-pedersen@regionstockholm.se>
2026-03-04 16:42:14 +0100

    gall

--- bollhav/__init__.py
+++ bollhav/__init__.py
@@ -2,5 +2,14 @@ from bollhav.model import Model
 from bollhav.modes import ModelType, WriteMode
 from bollhav.batching import BatchSize
 from bollhav.database import Database, DatabaseColumn
+from bollhav.intervals import TZInterval
 
-__all__ = ["Model", "ModelType", "WriteMode", "BatchSize", "Database", "DatabaseColumn"]
+__all__ = [
+    "Model",
+    "ModelType",
+    "WriteMode",
+    "BatchSize",
+    "Database",
+    "DatabaseColumn",
+    "TZInterval",
+]
