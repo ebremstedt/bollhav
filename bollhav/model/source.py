f23cc8cde4823878aaf2884893b8d01c1e79d852
ebremst3dt <erik.bremstedt-pedersen@regionstockholm.se>
2026-03-21 09:32:27 +0100

    gall

--- bollhav/model/source.py
+++ bollhav/model/source.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class Source:
     name: str
+    catalog: str | None = None
     schema: str | None = None
     partitioned_by: str | None = None
     dsn_env_var: str | None = None
