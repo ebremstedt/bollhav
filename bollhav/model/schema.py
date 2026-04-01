a0f4d7ae739aeb6034b82a4079ed3db1f3cd2950
ebremst3dt <erik.bremstedt-pedersen@regionstockholm.se>
2026-04-01 13:52:16 +0200

    gall

--- bollhav/model/schema.py
+++ bollhav/model/schema.py
@@ -13,6 +13,6 @@ class Schema:
         if self.suffix:
             s = self.name + "_" + self.suffix
             if self.suffix_appendix:
-                s = s + "_" + datetime.now().strftime(self.suffix_appendix)
+                s = s + "_" + datetime.now().strftime(self.suffix_appendix) + "_"
             return s
         return self.name
