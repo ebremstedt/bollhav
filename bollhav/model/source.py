d036321cae189fd70619e7fc14a4847883a1e7e8
Erik Bremstedt <59612986+ebremstedt@users.noreply.github.com>
2026-04-13 13:59:07 +0200

    gall

--- bollhav/model/source.py
+++ bollhav/model/source.py
@@ -9,4 +9,9 @@ class Source:
     partitioned_by: str | None = None
     dsn_env_var: str | None = None
     query: str | None = None
+    infer_schema_length: int | None = None
+    """Passed to polars as infer_schema_length. The maximum number of rows to scan
+    for schema inference. If set to None, the full data may be scanned (this can be
+    slow). This parameter only applies if the input data is a sequence or generator
+    of rows; other input is read as-is."""
     extra: dict | None = None
