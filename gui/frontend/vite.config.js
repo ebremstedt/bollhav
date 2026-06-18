138e4d27ecf4cb02e314c2c38c3895f034194144
Erik Bremstedt <59612986+ebremstedt@users.noreply.github.com>
2026-06-18 12:23:36 +0200

    gall

--- gui/frontend/vite.config.js
+++ gui/frontend/vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       "/graph": API,
+      "/config": API,
       "/environments": API,
       "/tree": API,
       "/lineage": API,
